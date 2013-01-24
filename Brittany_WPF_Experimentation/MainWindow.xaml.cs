da96dfae2e0e951f2deccda4e6db4130583f9ece
ahaddad <ahaddad@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-01-24 04:18:46 +0000

    Changed UserInterface Module to 64 bit applicationAdded individual Console Application Projects for each of us to experiment with the currently existing code base. Each project is preset with the correct namespace references and DLL files to allow for referencing the The main LASI Modules They are in the main solution under the folder:
    Individual_Experimentation
    
       Aluan_Experimentation
       Brittany_Experimentation + Brittany_WPF_Experimentation
       Dustin_Experimentation
       Erik_Experimentation
       Richard_Experimentation
       Scott_Experimentation
    
    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@18 49725315-0998-4498-bb4a-373bc1a52cb2

--- Brittany_WPF_Experimentation/MainWindow.xaml.cs
+++ Brittany_WPF_Experimentation/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using LASI.Algorithm;
+using LASI.FileSystem;
+
 namespace Brittany_WPF_Experimentation
 {
     /// <summary>
