ac93d2207ac97119ac3f055d27a607549b380c76
ahaddad <ahaddad@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-01-24 01:44:55 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@16 49725315-0998-4498-bb4a-373bc1a52cb2

---
+++ FileSystemAssemblyUnitTestProject/TextTagPairTest.cs
@@ -0,0 +1,92 @@
+﻿using LASI.FileSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FileSystemAssemblyUnitTestProject
+{
+
+
+    /// <summary>
+    ///This is a test class for TextTagPairTest and is intended
+    ///to contain all TextTagPairTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class TextTagPairTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext {
+            get {
+                return testContextInstance;
+            }
+            set {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Text
+        ///</summary>
+        [TestMethod()]
+        public void TextTest() {
+            TextTagPair target = new TextTagPair(); // TODO: Initialize to an appropriate value
+            string expected = string.Empty; // TODO: Initialize to an appropriate value
+            string actual;
+            target.Text = expected;
+            actual = target.Text;
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for Tag
+        ///</summary>
+        [TestMethod()]
+        public void TagTest() {
+            TextTagPair target = new TextTagPair(); // TODO: Initialize to an appropriate value
+            string expected = string.Empty; // TODO: Initialize to an appropriate value
+            string actual;
+            target.Tag = expected;
+            actual = target.Tag;
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
