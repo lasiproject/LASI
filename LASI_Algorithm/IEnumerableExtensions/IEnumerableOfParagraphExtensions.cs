fb7c91d98555dd61ffc56564d02a52a252b1a9a4
ahaddad <ahaddad@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-02-05 23:19:23 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@75 49725315-0998-4498-bb4a-373bc1a52cb2

--- LASI_Algorithm/IEnumerableExtensions/IEnumerableOfParagraphExtensions.cs
+++ LASI_Algorithm/IEnumerableExtensions/IEnumerableOfParagraphExtensions.cs
@@ -15,4 +15,21 @@ namespace LASI.Algorithm.IEnumerableExtensions
                    select r;
         }
     }
+    static class IEnumerableOfParagraphExtensions
+    {
+        public static IEnumerable<Sentence> GetSentences(this IEnumerable<Paragraph> paragraphs) {
+            return from p in paragraphs
+                   from s in p.Sentences
+                   select s;
+        }
+    }
+    static class IEnumerableOfParagraphExtensions
+    {
+        public static IEnumerable<Word> GetWords(this IEnumerable<Paragraph> paragraphs) {
+            return from p in paragraphs
+                   from s in p.Sentences
+                   from w in s.Words
+                   select w;
+        }
+    }
 }
