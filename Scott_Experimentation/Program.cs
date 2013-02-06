e9de707dd0790df231d8a1c039113c45c82c1abe
sminter <sminter@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-02-06 19:47:13 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@87 49725315-0998-4498-bb4a-373bc1a52cb2

--- Scott_Experimentation/Program.cs
+++ Scott_Experimentation/Program.cs
@@ -5,19 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 using LASI.Algorithm;
 using LASI.FileSystem;
+using SharpNLPTaggingModule;
 
 namespace Scott_Experimentation
 {
     class Program
     {
         static void Main(string[] args) {
-            Console.WriteLine("hello");
+            var tagger = new SharpNLPTagger(TaggingOption.TagAndAggregate, @"C:\Users\Scott\Desktop\TestSentences.txt");
+            var tagged = tagger.ProcessFile();
+            var paragraphs = new TaggedFileParser(tagged).GetParagraphs();
+            var document = new Document(paragraphs);
             
+           foreach (var p in document.Paragraphs)
+            {
+                foreach (var p1 in p.Sentences)
+                {
+                    Console.WriteLine(p1);
+                }
+            }
 
+            /*var para2 = from p in document.Paragraphs select p;
+             foreach (var p in para2) {
+                 var sents = from sent in p.Sentences select sent;
 
-
-            //Keeps the console window open until the escape key is pressed
-            Console.WriteLine("Press escape to exit");
+                 foreach (var s in sents){
+                     Console.WriteLine(s);
+                 }
+             }*/
+           
             for (var k = Console.ReadKey(); k.Key != ConsoleKey.Escape; k = Console.ReadKey()) {
                 Console.WriteLine("Press escape to exit");
             }
