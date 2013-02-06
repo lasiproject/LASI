adcff702b8cf87a983ed6ef3fe6bcf4f73c851bb
ahaddad <ahaddad@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-02-06 01:51:28 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@83 49725315-0998-4498-bb4a-373bc1a52cb2

--- Richard_Experimentation/Program.cs
+++ Richard_Experimentation/Program.cs
@@ -5,16 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using LASI.Algorithm;
 using LASI.FileSystem;
+using SharpNLPTaggingModule;
 
 namespace Richard_Experimentation
 {
     class Program
-    {
-        static void Main(string[] args) {
-
-
-
-
+    { static void Main(string[] args) {
+            
+            TagExampleFile();
 
             //Keeps the console window open until the escape key is pressed
             Console.WriteLine("Press escape to exit");
@@ -22,5 +20,28 @@ namespace Richard_Experimentation
                 Console.WriteLine("Press escape to exit");
             }
         }
+
+
+        static void TagExampleFile() {
+
+                                                                            //Put your windows account name here
+            var tagger = new SharpNLPTagger(TaggingOption.TagAndAggregate, @"C:\Richard\Desktop\intest1.txt");
+            var tagged = tagger.ProcessFile();
+            var paragraphs = new TaggedFileParser(tagged).GetParagraphs();
+            var document = new Document(paragraphs);
+
+            var para2 = from p in document.Paragraphs
+                        select p;
+            foreach (var p in para2) {
+                var phgrs = from sent in p.Sentences
+                            from r in sent.Phrases
+                            select r;
+
+                foreach (var r in phgrs) {
+                    Console.WriteLine(r);
+                }
+
+            }
+         }
     }
 }
