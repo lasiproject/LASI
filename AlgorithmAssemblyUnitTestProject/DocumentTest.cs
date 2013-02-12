a3975e3412f2244640f9cf0310b6d8765714a0fb
sminter <sminter@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-02-12 23:50:00 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@111 49725315-0998-4498-bb4a-373bc1a52cb2

---
+++ AlgorithmAssemblyUnitTestProject/DocumentTest.cs
@@ -0,0 +1,80 @@
+﻿using LASI.Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmAssemblyUnitTestProject
+{
+    
+    
+    /// <summary>
+    ///This is a test class for DocumentTest and is intended
+    ///to contain all DocumentTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class DocumentTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for PrintByLinkage
+        ///</summary>
+        [TestMethod()]
+        public void PrintByLinkageTest()
+        {
+            IEnumerable<Word> allWords = null; // TODO: Initialize to an appropriate value
+            Document target = new Document(allWords); // TODO: Initialize to an appropriate value
+            target.PrintByLinkage();
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+    }
+}
