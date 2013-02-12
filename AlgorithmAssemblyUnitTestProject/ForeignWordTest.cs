fbdfbdf444ba0fa29e1c79306b58311474a93873
sminter <sminter@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-02-12 23:48:58 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@108 49725315-0998-4498-bb4a-373bc1a52cb2

---
+++ AlgorithmAssemblyUnitTestProject/ForeignWordTest.cs
@@ -0,0 +1,94 @@
+﻿using LASI.Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AlgorithmAssemblyUnitTestProject
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ForeignWordTest and is intended
+    ///to contain all ForeignWordTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ForeignWordTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for ForeignWord Constructor
+        ///</summary>
+        [TestMethod()]
+        public void ForeignWordConstructorTest()
+        {
+            string text = "Bonjour"; 
+            ForeignWord target = new ForeignWord(text);
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for UsedAsType
+        ///</summary>
+        [TestMethod()]
+        public void UsedAsTypeTest()
+        {
+            string text = "Bonjour"; 
+            ForeignWord target = new ForeignWord(text); 
+            Type expected = typeof(Interjection);
+            Type actual;
+            target.UsedAsType = expected;
+            actual = target.UsedAsType;
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
