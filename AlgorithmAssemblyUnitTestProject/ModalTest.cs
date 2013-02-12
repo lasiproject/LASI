e2647bacbca322a84677f5ba9307b9e8ff9c5c6d
bjohnson <bjohnson@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-02-12 19:42:33 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@101 49725315-0998-4498-bb4a-373bc1a52cb2

---
+++ AlgorithmAssemblyUnitTestProject/ModalTest.cs
@@ -0,0 +1,94 @@
+﻿using LASI.Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AlgorithmAssemblyUnitTestProject
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ModalTest and is intended
+    ///to contain all ModalTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ModalTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Modal Constructor
+        ///</summary>
+        [TestMethod()]
+        public void ModalConstructorTest()
+        {
+            string text = string.Empty; // TODO: Initialize to an appropriate value
+            Modal target = new Modal(text);
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for Modifies
+        ///</summary>
+        [TestMethod()]
+        public void ModifiesTest()
+        {
+            string text = string.Empty; // TODO: Initialize to an appropriate value
+            Modal target = new Modal(text); // TODO: Initialize to an appropriate value
+            IModalityModifiable expected = null; // TODO: Initialize to an appropriate value
+            IModalityModifiable actual;
+            target.Modifies = expected;
+            actual = target.Modifies;
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
