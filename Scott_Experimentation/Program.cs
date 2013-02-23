1b53c0ce21db2ca174a95a868af98321392b64fc
sminter <sminter@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-02-23 13:14:50 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@193 49725315-0998-4498-bb4a-373bc1a52cb2

--- Scott_Experimentation/Program.cs
+++ Scott_Experimentation/Program.cs
@@ -20,15 +20,17 @@ namespace Scott_Experimentation
             var paragraphs = new TaggedFileParser(tagged).LoadParagraphs();
             var document = new Document(paragraphs);
 
-            Word w1 = document.WordAt(5);
+            string sep = "\n***************************************************************\n";
+
+            Word w1 = document.WordAt(2003736);
             Console.WriteLine("Word at 5: {0}", w1.Text);
-            var strng1 = document.WordTextAt(5);
-            Console.WriteLine("Word Text at 5: {0}", strng1);
+            var strng1 = document.WordTextAt(500);
+            Console.WriteLine("Word Text at 5: {0}{1}", strng1, sep);
 
             Sentence sent1 = document.SentenceAt(1);
             Console.WriteLine("Sentence at 1: {0}", sent1.Text);
             string strng2 = document.SentenceTextAt(1);
-            Console.WriteLine("Sentence text at 1: {0}", strng2);
+            Console.WriteLine("Sentence text at 1: {0}{1}", strng2, sep);
 
             /*
             for (var x = 0; x < document.Words.Count(); x++)
