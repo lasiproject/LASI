5561e71ec50df8d95c9302dc11d92e380ce13d32
ahaddad <ahaddad@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-03-04 21:19:21 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@259 49725315-0998-4498-bb4a-373bc1a52cb2

--- LASI_Algorithm/InvalidStateTransitionException.cs
+++ LASI_Algorithm/InvalidStateTransitionException.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace LASI.Algorithm.ObjectBinding
+namespace LASI.Algorithm
 {
     class InvalidStateTransitionException : Exception
     {
