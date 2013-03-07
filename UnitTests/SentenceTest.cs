9d406af0dff9095271d8504657fd827827d55736
rowens <rowens@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-03-07 14:29:29 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@309 49725315-0998-4498-bb4a-373bc1a52cb2

--- UnitTests/SentenceTest.cs
+++ UnitTests/SentenceTest.cs
@@ -70,9 +70,9 @@ namespace AlgorithmAssemblyUnitTestProject
         [TestMethod()]
         public void ToStringTest()
         {
-            Phrase[] phrases = {new NounPhrase(new Word ( new ProperSingularNoun("LASI"))), new VerbPhrase (new Word (new PastTenseVerb ("found"))), new NounPhrase (new Word (new ProperPluralNoun ("TIMIS")))}; // TODO: Initialize to an appropriate value
+            Phrase[] phrases = new[] {new NounPhrase[]{new Word[] {new ProperSingularNoun("LASI")}}, new VerbPhrase[] {new Word[] {new PastTenseVerb ("found")}}, new NounPhrase[] {new Word[] {new ProperPluralNoun ("TIMIS")}}}; // TODO: Initialize to an appropriate value
             Sentence target = new Sentence(phrases); // TODO: Initialize to an appropriate value
-            string expected = "LASI finds TIMIS"; // TODO
+            string expected = "LASI found TIMIS"; // TODO
             Assert.AreEqual(expected, actual);
             //Assert.Inconclusive("Verify the correctness of this test method.");
         } 
