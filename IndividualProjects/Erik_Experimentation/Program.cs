8afacf6e37afe018754c5868367ce7f08531dfd2
eroger <eroger@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-03-19 23:20:04 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@345 49725315-0998-4498-bb4a-373bc1a52cb2

--- IndividualProjects/Erik_Experimentation/Program.cs
+++ IndividualProjects/Erik_Experimentation/Program.cs
@@ -24,17 +24,33 @@ namespace Erik_Experimentation
         static void Main(string[] args)
         {
 
-                   var nounTest = new LASI.Algorithm.Thesauri.NounThesaurus(@"..\..\..\..\WordNetThesaurusData\data.noun");
-                   nounTest.Load();
-                   string key = "give-and-take";
-                   nounTest.SearchFor(key);
+                   //var nounTest = new LASI.Algorithm.Thesauri.NounThesaurus(@"..\..\..\..\WordNetThesaurusData\data.noun");
+                   //nounTest.Load();
+                   //string key = "walk";
+                   //nounTest.SearchFor(key);
 
 
 
 
-            //Keeps the console window open until the escape key is pressed
+                    //Keeps the console window open until the escape key is pressed
+                    //Console.WriteLine("Press escape to exit");
+                    //for (var k = Console.ReadKey(); k.Key != ConsoleKey.Escape; k = Console.ReadKey()) {
+                        //Console.WriteLine("Press escape to exit");
+                    //}
+
+            var tagger = new SharpNLPTagger(TaggingOption.TagAndAggregate, @"C:\Users\CynosureEPR\Desktop\test.txt");
+            var tagged = tagger.ProcessFile();
+            var paragraphs = new TaggedFileParser(tagged).LoadParagraphs();
+            var document = new Document(paragraphs);
+            StreamWriter file = new StreamWriter(@"C:\Users\CynosureEPR\Desktop\output.txt");
+
+            foreach (var i in document.Phrases)
+            {
+                Console.WriteLine(i);
+            }
             Console.WriteLine("Press escape to exit");
-            for (var k = Console.ReadKey(); k.Key != ConsoleKey.Escape; k = Console.ReadKey()) {
+            for (var k = Console.ReadKey(); k.Key != ConsoleKey.Escape; k = Console.ReadKey())
+            {
                 Console.WriteLine("Press escape to exit");
             }
         }
