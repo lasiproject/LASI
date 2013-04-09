1b8bfe628b8cf14fa84cc82433709a9338fd0966
sminter <sminter@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-04-09 01:06:15 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@447 49725315-0998-4498-bb4a-373bc1a52cb2

--- LASI_Algorithm/Analysis/Binding/InterPhraseWordBinding.cs
+++ LASI_Algorithm/Analysis/Binding/InterPhraseWordBinding.cs
@@ -18,35 +18,39 @@ namespace LASI.Algorithm.Binding
 
 
             if (np.Words.Count() > 1 && LastNoun != null) {
+                
 
-
-
-                //foreach (Word w in np.Words) {
-                //    Console.Write("[{0}] ", w);
-                //}
-                //Console.Write("\nLast Noun: {0}", LastNoun);
+                
+                foreach (Word w in np.Words)
+                {
+                    Console.Write("[{0}] ", w);
+                }
+                //Console.Write("\nLast Noun: {0}", LastNoun.Text);
                 //Console.Write("\n------\n");
-
+                
 
 
                 /**
                  * Binding determiners to last noun
                  */
                 Determiner det1 = np.Words.OfType<Determiner>().FirstOrDefault();
-                if (det1 != null) {
+                if (det1 != null)
+                {
                     LastNoun.BindDeterminer(det1);
                     det1.Determines = LastNoun;
-                    //Console.WriteLine("Last Noun: {0}, Determined By: {1}", LastNoun.Text, LastNoun.DeterminedBy.Text);
-                    //Console.WriteLine("Determiner: {0}, Determines: {1}", det1.Text, det1.Determines.Text);
+                    Console.WriteLine("Last Noun: {0}, Determined By: {1}", LastNoun.Text, LastNoun.DeterminedBy.Text);
+                    Console.WriteLine("Determiner: {0}, Determines: {1}", det1.Text, det1.Determines.Text);
                 }
 
-
+            
                 /**
                  * Binding Adjectives to last noun
                  */
                 var ListOfAdjectives = np.Words.GetAdjectives();
-                if (ListOfAdjectives.Count() > 0) {
-                    foreach (Adjective adj in ListOfAdjectives) {
+                if (ListOfAdjectives.Count() > 0)
+                {
+                    foreach (Adjective adj in ListOfAdjectives)
+                    {
                         LastNoun.BindDescriber(adj);
                         adj.Described = LastNoun;
                     }
@@ -59,12 +63,12 @@ namespace LASI.Algorithm.Binding
                     }
                     Console.WriteLine('\n');
                     */
-                    /*
+                    
                     foreach (Adjective adj in ListOfAdjectives)
                     {
                         Console.WriteLine("Adjective: {0}, Describes: {1}", adj.Text, adj.Described.Text);
                     }
-                    */
+                    
                 }
 
 
@@ -73,32 +77,36 @@ namespace LASI.Algorithm.Binding
                  *  Binding first posessive pronoun to last noun
                  */
                 var PosNoun = np.Words.OfType<PossessivePronoun>().FirstOrDefault();
-                if (PosNoun != null) {
+                if (PosNoun != null)
+                {
                     PosNoun.AddPossession(LastNoun);
-
-
-                    /*
+                    
+                    
+                    
                     Console.Write("Pronoun: {0} => ", PosNoun.Text);
                     foreach (var p in PosNoun.Possessed)
                     {
                         Console.Write("  {0}", p.Text);
                     }
                     Console.Write("\n");
-                    */
+                    
                 }
+
+                Console.WriteLine("\n~~~~~~~~~~~~~~~~~\n");
             }
         }
 
 
-        public void IntraVerbPhrase(VerbPhrase vp) {
+        public void IntraVerbPhrase(VerbPhrase vp)
+        {
             Verb LastVerb = vp.Words.OfType<Verb>().LastOrDefault();
-            if (vp.Words.Count() > 1 && LastVerb != null) {
-                //    foreach (Word w in vp.Words)
-                //    {
-                //        Console.Write("{0}, ", w);
-                //    }
-                //    Console.WriteLine("\n~~~~~~~~~~~~~~~~~\n");
-                //
+            if (vp.Words.Count() > 1 && LastVerb != null)
+            {
+                foreach (Word w in vp.Words)
+                {
+                    Console.Write("{0}, ", w);
+                }
+                Console.WriteLine("\n~~~~~~~~~~~~~~~~~\n");
             }
         }
     }
