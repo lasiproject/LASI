a3c91f37073c926b3f3479f8daf61a1491cc905c
dpatrick <dpatrick@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-04-15 21:27:25 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@473 49725315-0998-4498-bb4a-373bc1a52cb2

--- LASI_Algorithm/Analysis/Weighter.cs
+++ LASI_Algorithm/Analysis/Weighter.cs
@@ -14,36 +14,50 @@ namespace LASI.Algorithm.Analysis
         /// <param name="doc"></param>
         static public void weight(Document doc)
         {
-            var wordsByCount = from w in doc.Words group w by new { w.Type, w.Text };
-            var phraseByCount = from p in doc.Phrases group p by new { Type = p.GetType(), p.Text };
+            var wordsByCount = from w in doc.Words
+                               where !(w is Preposition)
+                               where !(w is Determiner)
+                               where !(w is Conjunction)
+                               group w by new
+                               {
+                                   w.Type,
+                                   w.Text
+                               };
+
+            var phraseByCount = from p in doc.Phrases
+                                group p by new
+                                {
+                                    Type = p.GetType(),
+                                    p.Text
+                                };
+
             var nounSynonymGroups = from w in doc.Words.GetNouns()
                                     let synstrings = Thesauri.ThesaurusManager.NounThesaurus[w]
                                     from t in doc.Words.GetNouns()
                                     where synstrings.Contains(t.Text)
                                     group t by w;
+
             var verbsynonymgroups = from w in doc.Words.GetVerbs()
                                     let synstrings = Thesauri.ThesaurusManager.VerbThesaurus[w]
                                     from t in doc.Words.GetVerbs()
                                     where synstrings.Contains(t.Text)
                                     group t by w;
 
-            var basicPhraseCount = from p in doc.
+            var phraseWeightPairs = from p in doc.Phrases.AsParallel()
+                                    let weight = p.Words.Average(w => w.Weight)
+                                    select new
+                                    {
+                                        p,
+                                        weight
+                                    };
 
-            //basic word count by part of speech 
+
+            //basic word count by part of speech ignoring determiners and conjunctions
             foreach (var grp in wordsByCount)
             {
                 foreach (var w in grp)
                 {
                     w.Weight = grp.Count();
-                }
-            }
-
-            //basic phrase count by part of speech
-            foreach (var grp in phraseByCount)
-            {
-                foreach (var p in grp)
-                {
-                    p.Weight = grp.Count();
                 }
             }
 
@@ -58,6 +72,23 @@ namespace LASI.Algorithm.Analysis
             {
                 grp.Key.Weight += 0.7m * grp.Count();
             }
+
+            //assign weights to phrases 
+            foreach (var pair in phraseWeightPairs)
+            {
+                pair.p.Weight = pair.weight;
+            }
+
+            //increment weight of identical phrases
+            foreach (var grp in phraseByCount)
+            {
+                foreach (var p in grp)
+                {
+                    p.Weight += grp.Count();
+                }
+            }
+
+
         }
     }
 }
