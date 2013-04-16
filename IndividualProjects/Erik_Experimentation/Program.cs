bcacb6195490ef3d9ba8c4264d76783d441656b6
eroger <eroger@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-04-16 21:30:42 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@484 49725315-0998-4498-bb4a-373bc1a52cb2

--- IndividualProjects/Erik_Experimentation/Program.cs
+++ IndividualProjects/Erik_Experimentation/Program.cs
@@ -14,6 +14,7 @@ using LASI.Algorithm;
 using LASI.FileSystem;
 using LASI.Algorithm.Weighting;
 using LASI.Algorithm.Thesauri;
+using System.Text.RegularExpressions;
 
 
 namespace Erik_Experimentation
@@ -23,10 +24,10 @@ namespace Erik_Experimentation
 
         static void Main(string[] args) {
 
-            var nounTest = new NounThesaurus(@"..\..\..\..\WordNetThesaurusData\data.noun");
-            nounTest.Load();
-            string key = "object";
-            nounTest.SearchFor(key);
+            //var nounTest = new NounThesaurus(@"..\..\..\..\WordNetThesaurusData\data.noun");
+            //nounTest.Load();
+            //string key = "object";
+            //nounTest.SearchFor(key);
 
             //var adjTest = new AdjectiveThesaurus(@"..\..\..\..\WordNetThesaurusData\data.adj");
             //adjTest.Load();
@@ -39,6 +40,275 @@ namespace Erik_Experimentation
             //advTest.SearchFor(key);
 
             //Keeps the console window open until the escape key is pressed
+            //Console.WriteLine("Press escape to exit");
+            //for (var k = Console.ReadKey(); k.Key != ConsoleKey.Escape; k = Console.ReadKey())
+            //{
+            //    Console.WriteLine("Press escape to exit");
+            //}
+
+            var tagger = new SharpNLPTagger(TaggingOption.TagAndAggregate, @"C:\Users\CynosureEPR\Desktop\weight2.txt");
+            var tagged = tagger.ProcessFile();
+            var paragraphs = new TaggedFileParser(tagged).LoadParagraphs();
+            var document = new Document(paragraphs);
+
+            //foreach (var i in document.Phrases)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //foreach (var i in document.Words)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+
+
+            //SIX PHASES
+                //PHASE 1 - Standard word weight based on part of speech (standardization)
+                //PHASE 2 - Word weight based on part of speech and neighbors' (+2) part of speech
+                //PHASE 3 - Standard phrase weight based on phrase part of speech (standardization)
+                //PHASE 4 - Phrase weight based on part of speech and neibhors' (full sentence) part of speech
+                //PHASE 5 - FREQUENCIES
+                        // .1 - Frequency of Word/Phrase in document
+                        // .2 - Frequency of Word/Phrase in document compared to other documents in set
+                //PHASE 6 - SYNONYMS
+                        // .1 - Frequency of Word (/Phrase?) in document
+                        // .2 - Frequency of Word (/Phrase?) in document compared to other documents in set
+
+
+ 
+                int primary, secondary, tertiary, quaternary, quinary, senary;
+                int based = 10;
+                primary = (secondary = (tertiary = (quaternary = (quinary = (senary = 0) + based) + based) + based) + based) + based;
+
+//PHASE 1 - Standard word weight based on part of speech (standardization)
+
+            Console.WriteLine("Standard Word weight based on POS:");
+            foreach (Sentence s in document.Sentences)
+            {
+                //Console.WriteLine(s);
+                
+                foreach (Word w in s.Words)
+                {
+                    Console.WriteLine(w);
+
+                    
+
+                    if (w is Noun)
+                    {
+                        w.Weight = primary;
+                    }
+                    else if (w is Verb || w is PastParticipleVerb)
+                    {
+                        w.Weight = secondary;
+                    }
+                    else if (w is Adjective)
+                    {
+                        w.Weight = tertiary;
+                    }
+                    else if (w is Adverb)
+                    {
+                        w.Weight = quaternary;
+                    }
+                    else if (w is Pronoun)
+                    {
+                        w.Weight = quinary;
+                    }
+                    else
+                    {
+                        w.Weight = senary;
+                    }
+
+
+
+
+                    Console.WriteLine(w.Weight);
+                    
+                }
+
+                
+            }
+
+
+
+
+
+//PHASE 2 - Word weight based on part of speech and neighbors' (+2) part of speech
+            // WORKS, BUT
+             // NEED FORMULAS FOR MODIFIER VARIABLES - WHAT SHOULD THESE BE?
+
+            //foreach (Sentence s in document.Sentences)
+            //{
+            //    //Console.WriteLine(s);
+
+            //    foreach (Word w in s.Words)
+            //    {
+
+            //        Word next = w ?? w.NextWord;
+            //        Word nextNext = next ?? next.NextWord;
+
+            //        Word prev = w ?? w.PreviousWord;
+            //        Word prevPrev = prev ?? prev.PreviousWord;
+
+            //        //nouns
+            //        if (w is Noun)
+            //        {
+            //            if (next == w)
+            //            {
+                          
+            //            }
+            //            else if (next is Noun)
+            //            {
+            //                w.Weight = next.Weight = CompoundNounModifier;
+            //            }
+            //            else if (next is Verb)
+            //            {
+            //                w.Weight = next.Weight = NounActionModifier;
+            //            }
+            //            else if (next is Adverb)
+            //            {
+            //                w.Weight = next.Weight = NounAActionModifier;
+            //            }
+
+            //        }
+                    
+            //    }
+            
+            
+            //}
+
+
+
+
+//PHASE 3 - Standard phrase weight based on phrase part of speech (standardization)
+
+
+
+
+//PHASE 4 - Phrase weight based on part of speech and neibhors' (full sentence) part of speech
+
+
+
+//PHASE 5 - FREQUENCIES
+    // .1 - Frequency of Word in document
+            Console.WriteLine(" ");
+            Console.WriteLine("Word Frequencies in Document:");
+
+            foreach (Word w in document.Words){
+
+                
+
+                foreach (Word w1 in document.Words)
+                {
+                   
+                    if (w.Text == w1.Text)  //why doesn't 'w1.Equals(current)' work?
+                    {
+                        w1.FrequencyCurrent += 1;
+                    }
+                }
+
+                
+
+            }
+
+            foreach (Word w in document.Words)
+            {
+                Console.WriteLine(w);
+                Console.WriteLine(w.FrequencyCurrent);
+                float percentID = (w.FrequencyCurrent / (float)document.Words.Count());
+                Console.WriteLine("Frequency % in document:" + percentID);
+            } //NEED FORMULA TO MODIFY WEIGHT
+
+            
+
+// .2 - Frequency of Word in document compared to other documents in set
+           // NEED FORUMULA FOR THIS
+
+//PHASE 6 - SYNONYMS
+// .1 - Frequency of Word in document
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Word Synonyms in Document:");
+            IEnumerable<Noun> nouns = document.Words.GetNouns();
+
+            IEnumerable<string> synonyms;
+
+            foreach (var n in nouns)
+            {
+                var nounTest = new NounThesaurus(@"..\..\..\..\WordNetThesaurusData\data.noun");
+
+                nounTest.Load();
+                synonyms = nounTest.SearchFor(n.Text);
+
+
+
+
+                foreach (Word w in document.Words)
+                {
+                    foreach (var s in synonyms)
+                    {
+                        if (w.Text == s)
+                        {
+                            w.Synonyms += 1;
+                        }
+                    }
+
+
+
+
+
+                    //NEED REAL FORMULA FOR WEIGHT MODIFICATION
+                    w.Weight += w.Synonyms;
+
+
+
+
+                    Console.WriteLine(w);
+                    Console.WriteLine(w.Synonyms);
+                    Console.WriteLine(w.Weight);
+
+                }
+            }
+
+
+
+            //var adjTest = new AdjectiveThesaurus(@"..\..\..\..\WordNetThesaurusData\data.adj");
+            //adjTest.Load();
+            //string key = "able";
+            //adjTest.SearchFor(key);
+
+            //var advTest = new AdverbThesaurus(@"..\..\..\..\WordNetThesaurusData\data.adv");
+            //advTest.Load();
+            //string key = "vastly";
+            //advTest.SearchFor(key);
+
+// .2 - Frequency of Word in document compared to other documents in set
+          
+         
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             Console.WriteLine("Press escape to exit");
             for (var k = Console.ReadKey(); k.Key != ConsoleKey.Escape; k = Console.ReadKey()) {
                 Console.WriteLine("Press escape to exit");
@@ -57,6 +327,7 @@ namespace Erik_Experimentation
             //for (var k = Console.ReadKey(); k.Key != ConsoleKey.Escape; k = Console.ReadKey()) {
             // Console.WriteLine("Press escape to exit");
             //}
+
         }
     }
 }
