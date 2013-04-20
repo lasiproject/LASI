28c245ac9882f1b7d2fa4aec18f8e5cdea57378c
bjohnson <bjohnson@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-04-20 12:35:44 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@514 49725315-0998-4498-bb4a-373bc1a52cb2

--- LASI_UserInterface/ResultsScreen.xaml.cs
+++ LASI_UserInterface/ResultsScreen.xaml.cs
@@ -19,6 +19,7 @@ using System.Collections.ObjectModel;
 using LASI.Algorithm.Binding;
 using LASI.Utilities.TypedSwitch;
 using LASI.Algorithm.Analysis;
+using LASI.Utilities;
 
 namespace LASI.UserInterface
 {
@@ -27,18 +28,25 @@ namespace LASI.UserInterface
     /// </summary>
     public partial class ResultsScreen : Window
     {
-        public ResultsScreen() {
+        public ResultsScreen()
+        {
 
             InitializeComponent();
             this.Closed += (s, e) => Application.Current.Shutdown();
 
         }
 
+        //WORD RELATIONSHIPS TAB//
 
-        public void BuildAssociationTextView() {
-            foreach (var doc in documents) {
-                foreach (var phrase in doc.Phrases) {
-                    var phraseLabel = new Label {
+        public void BuildAssociationTextView()
+        {  // This is for the word relationships tab
+            foreach (var doc in documents)
+            {
+                var phraseLabels = new List<Label>();
+                foreach (var phrase in doc.Phrases)
+                {
+                    var phraseLabel = new Label
+                    {
                         Content = phrase.Text,
                         Tag = phrase,
                         Foreground = Brushes.Black,
@@ -47,45 +55,57 @@ namespace LASI.UserInterface
                         ToolTip = phrase.Type.Name,
                     };
                     var vP = phrase as VerbPhrase;
-                    if (vP != null && vP.BoundSubjects.Count() > 0) {
-                        var visitSubjectMI = new MenuItem {
+                    if (vP != null && vP.BoundSubjects.Count() > 0)
+                    {
+                        var visitSubjectMI = new MenuItem
+                        {
                             Header = "view subjects"
                         };
-                        visitSubjectMI.Click += (sender, e) => {
+                        visitSubjectMI.Click += (sender, e) =>
+                        {
                             var objlabels = from r in vP.BoundSubjects
                                             join l in phraseLabels on r equals l.Tag
                                             select l;
-                            foreach (var l in objlabels) {
+                            foreach (var l in objlabels)
+                            {
                                 l.Foreground = Brushes.Black;
                                 l.Background = Brushes.Red;
                             }
                         };
                         phraseLabel.ContextMenu.Items.Add(visitSubjectMI);
                     }
-                    if (vP != null && vP.DirectObjects.Count() > 0) {
-                        var visitSubjectMI = new MenuItem {
+                    if (vP != null && vP.DirectObjects.Count() > 0)
+                    {
+                        var visitSubjectMI = new MenuItem
+                        {
                             Header = "view direct objects"
                         };
-                        visitSubjectMI.Click += (sender, e) => {
+                        visitSubjectMI.Click += (sender, e) =>
+                        {
                             var objlabels = from r in vP.DirectObjects
                                             join l in phraseLabels on r equals l.Tag
                                             select l;
-                            foreach (var l in objlabels) {
+                            foreach (var l in objlabels)
+                            {
                                 l.Foreground = Brushes.Black;
                                 l.Background = Brushes.Red;
                             }
                         };
                         phraseLabel.ContextMenu.Items.Add(visitSubjectMI);
                     }
-                    if (vP != null && vP.IndirectObjects.Count() > 0) {
-                        var visitSubjectMI = new MenuItem {
+                    if (vP != null && vP.IndirectObjects.Count() > 0)
+                    {
+                        var visitSubjectMI = new MenuItem
+                        {
                             Header = "view indirect objects"
                         };
-                        visitSubjectMI.Click += (sender, e) => {
+                        visitSubjectMI.Click += (sender, e) =>
+                        {
                             var objlabels = from r in vP.IndirectObjects
                                             join l in phraseLabels on r equals l.Tag
                                             select l;
-                            foreach (var l in objlabels) {
+                            foreach (var l in objlabels)
+                            {
                                 l.Foreground = Brushes.Black;
                                 l.Background = Brushes.Red;
                             }
@@ -94,32 +114,38 @@ namespace LASI.UserInterface
                     }
                     phraseLabels.Add(phraseLabel);
                 }
-                foreach (var l in phraseLabels) {
+                foreach (var l in phraseLabels)
+                {
                     AssociationPhrasePanal.Children.Add(l);
                 }
             }
-        }
+        } // end word relationships
 
 
 
 
-
-        public void BuildFullSortedView() {
-            foreach (var doc in documents) {
+        public void BuildFullSortedView()
+        { //Build 
+            foreach (var doc in documents)
+            {
                 var wordLabels = new List<Label>();
                 var words = doc.Words.GetNouns().Concat<Word>(doc.Words.GetAdverbs()).
                      Concat<Word>(doc.Words.GetAdjectives()).Concat<Word>(doc.Words.GetVerbs()).
-                     GroupBy(w => new {
+                     GroupBy(w => new
+                     {
                          w.Text,
                          w.Type
                      }).Select(g => g.First());
 
-                foreach (var word in words) {
+                foreach (var word in words)
+                {
                     var wordLabel = MakeWordLabel(word);
-                    var menuItem1 = new MenuItem {
+                    var menuItem1 = new MenuItem
+                    {
                         Header = "view definition",
                     };
-                    menuItem1.Click += (sender, e) => {
+                    menuItem1.Click += (sender, e) =>
+                    {
                         Process.Start(String.Format("http://www.dictionary.reference.com/browse/{0}?s=t", word.Text));
                     };
                     wordLabel.ContextMenu.Items.Add(menuItem1);
@@ -129,14 +155,8 @@ namespace LASI.UserInterface
 
                 }
 
-                ValueList = new List<KeyValuePair<string, int>>();
-                ValueList.AddRange(
-                    (from w in words
-                     orderby w.Weight descending
-                     select new KeyValuePair<string, int>(w.Text, (int) w.Weight)).Take(15).ToList());
-                ValueList.Reverse();
-                lineChart.DataContext = ValueList;
 
+                MakeFrequencyChart(doc);
 
 
 
@@ -145,28 +165,89 @@ namespace LASI.UserInterface
                 s.Content = stackPanel;
                 var grid = new Grid();
                 grid.Children.Add(s);
-                var tabItem = new TabItem {
+                var tabItem = new TabItem
+                {
                     Header = doc.FileName,
                     Content = grid
                 };
                 foreach (var l in from w in wordLabels
                                   orderby (w.Tag as Word).Weight descending
-                                  select w) {
+                                  select w)
+                {
                     stackPanel.Children.Add(l);
                 }
-                TestView.Items.Add(tabItem);
+                WordCountLists.Items.Add(tabItem);
 
 
             }
 
-        }
-        public List<KeyValuePair<string, int>> ValueList {
-            get;
-            private set;
+        } //end build assorted view
+
+
+
+
+        public void MakeFrequencyChart(Document docu)
+        {
+
+            var words = docu.Words.GetNouns().Concat<Word>(docu.Words.GetAdverbs()).
+                   Concat<Word>(docu.Words.GetAdjectives()).Concat<Word>(docu.Words.GetVerbs()).
+                   GroupBy(w => new
+                   {
+                       w.Text,
+                       w.Type
+                   }).Select(g => g.First());
+
+            var valueList = new List<KeyValuePair<string, int>>();
+            valueList.AddRange(
+                (from w in words
+                 orderby w.Weight descending
+                 select new KeyValuePair<string, int>(w.Text, (int)w.Weight))
+                .Take(15)
+                .ToList());
+
+
+            valueList.Reverse();
+
+            var chart = new Chart
+            {
+                Name = docu.FileName,
+                Title = docu.FileName + "Top Words",
+
+
+            };
+            Series series = new BarSeries
+            {
+
+                DependentValuePath = "Value",
+                IndependentValuePath = "Key",
+                ItemsSource = valueList,
+                IsSelectionEnabled = true,
+
+            };
+            chart.Series.Add(series);
+
+            var tabItem = new TabItem
+            {
+                Header = docu.FileName,
+                Content = chart
+            };
+            FrequencyCharts.Items.Add(tabItem);
         }
 
-        private static Label MakeWordLabel(Word word) {
-            var wordLabel = new Label {
+
+        /// <summary>
+        /// Creates a label UI element for a word
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns>
+        /// New label element whose tag properties is assigned to the given word
+        /// </returns>
+
+
+        private static Label MakeWordLabel(Word word)
+        {
+            var wordLabel = new Label
+            {
                 Tag = word,
                 Content = String.Format("Weight : {0}  \"{1}\"", word.Weight, word.Text),
                 Foreground = Brushes.Black,
@@ -177,83 +258,73 @@ namespace LASI.UserInterface
             return wordLabel;
         }
 
-        //private void BuildUniqueClickAction(Label wordLabel) {
-        //    wordLabel.MouseDown += (sender, e) => {
-        //        var intraPhraseLabels = from w in (wordLabel.Tag as Word).ParentPhrase.Words
-        //                                join l in wordLabels on w.ID equals (l.Tag as Word).ID
-        //                                select l;
-        //        foreach (var l in intraPhraseLabels) {
-        //            if (l.Background != Brushes.Green && wordLabel.Foreground != Brushes.Red) {
-        //                l.Foreground = Brushes.White;
-        //                l.Background = Brushes.Green;
-        //                wordLabel.Foreground = Brushes.Black;
-        //                wordLabel.Background = Brushes.Red;
-
-        //            } else {
-        //                l.Background = Brushes.White;
-        //                l.Foreground = Brushes.Black;
-        //                wordLabel.Foreground = Brushes.Black;
-        //                wordLabel.Background = Brushes.White;
-        //            }
-        //        }
-
-        //    };
-        //}
+   
 
 
         private List<Document> documents = new List<Document>();
 
-        public List<Document> Documents {
-            get {
+        public List<Document> Documents
+        {
+            get
+            {
                 return documents;
             }
-            set {
+            set
+            {
                 documents = value;
             }
         }
 
-        private List<Label> phraseLabels = new List<Label>();
+        private List<Series> DocumentDatapointRanges = new List<Series>();
 
-        private void MenuItem_Click_2(object sender, RoutedEventArgs e) {
+
+
+
+
+
+
+
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
 
             this.SwapWith(WindowManager.CreateProjectScreen);
         }
 
-        private void MenuItem_Click_3(object sender, RoutedEventArgs e) {
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
             this.Close();
 
         }
 
-        List<string> tokens = @"The typically small numbers of COIs identified by stakeholders with specific problems should each be accompanied by a similarly small number
-` effectiveness measures (Sproles, 2002) that stakeholders can use to make binary, 'yes' or 'no,' determinations as to whether or not COI and 
-the problems they characterize have been resolved. Recalling their definition as 'emergent properties that induce rank orderings,' 
-MOEs are truly the 'standards' of the same definition as well as, perhaps most plainly, the variables of 'goodness' described by Dockery (1986, p. 172).
-Their complete independence from solutions proposed to dispel the problems from which they fundamentally derive represents another of MOEs' 
-most significant features, one reinforced with the Figure 3 that builds on Figure 2 by displaying only those entities - the problem and 
-the COIs characterizing it - to which MOEs may be properly linked.".Split('\r', '\n', '\t', ' ').ToList();
 
-
-
-        private void printButton_Click_1(object sender, RoutedEventArgs e) {
+        private void printButton_Click_1(object sender, RoutedEventArgs e)
+        {
             var printDialog = new PrintDialog();
             printDialog.ShowDialog();
-            printDialog.PrintVisual(resultsGrid, "Current View");
+            // printDialog.PrintVisual(resultsGrid, "Current View");
         }
 
-        private void Grid_MouseDown_1(object sender, MouseButtonEventArgs e) {
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e) {
+        private void Grid_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
 
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
 
+        }
 
-
-        public bool AutoExport {
+        public bool AutoExport
+        {
             get;
             set;
+        }
+
+        private void changeChartType_Column_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
