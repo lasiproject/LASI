cc43b7d24c924459c2b14cbbecbb07afb7c02449
ahaddad <ahaddad@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-04-21 14:46:06 +0000

    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@523 49725315-0998-4498-bb4a-373bc1a52cb2

---
+++ LASI_FileSystem/Serialization/XML/ILexicalWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LASI.FileSystem.Serialization.XML
+{
+    public interface ILexicalWriter<in S, out T, in W> : IDisposable
+        where W : System.Xml.XmlWriter
+        where T : LASI.Algorithm.ILexical
+        where S : IEnumerable<LASI.Algorithm.ILexical>
+    {
+        void Write(S resultSet, string resultSetTitle, DegreeOfOutput degreeOfOutput);
+        Task WriteAsync(S resultSet, string resultSetTitle, DegreeOfOutput degreeOfOutput);
+        System.Xml.XmlWriter Target {
+            get;
+        }
+    }
+    public enum DegreeOfOutput
+    {
+        TopWeighted,
+        Graphed,
+        Comprehensive,
+        ContextSpecific
+    }
+}
---
+++ LASI_FileSystem/Serialization/XML/SimpleLexicalSerializer.cs
@@ -0,0 +1,66 @@
+﻿using LASI.Algorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LASI.FileSystem.Serialization.XML
+{
+    public class SimpleLexicalSerializer : ILexicalWriter<IEnumerable<ILexical>, ILexical, XmlWriter>
+    {
+        public SimpleLexicalSerializer()
+            : this(Console.Out) {
+        }
+        public SimpleLexicalSerializer(XmlWriter target) {
+            Target = target;
+        }
+        public SimpleLexicalSerializer(string uri) {
+            Target = XmlWriter.Create(uri);
+        }
+        public SimpleLexicalSerializer(System.IO.TextWriter textWriter) {
+            Target = XmlWriter.Create(textWriter);
+        }
+        public void Write(IEnumerable<ILexical> resultSet, string resultSetTitle, DegreeOfOutput degreeOfOutput) {
+            var serializedResults =
+                Serialize(resultSet, resultSetTitle, degreeOfOutput);
+            serializedResults.Save(Target);
+        }
+
+        private XElement Serialize(IEnumerable<ILexical> resultSet, string resultSetTitle, DegreeOfOutput degreeOfOutput) {
+            return new XElement("Root",
+                            new XElement("Results",
+                                new XAttribute("Title", resultSetTitle),
+                                new XAttribute("Range", degreeOfOutput),
+                            from l in resultSet
+                            select new XElement(l.Type.Name,
+                                new XAttribute("Text", l.Text),
+                                new XElement("Weights",
+                                    new XElement("Weight",
+                                        new XAttribute("Level", "Document"), l.Weight),
+                                new XElement("Weight",
+                                    new XAttribute("Level", "Crossed"), l.MetaWeight)
+                                    )
+                                )
+                            )
+                        );
+        }
+
+        public XmlWriter Target {
+            get;
+            protected set;
+        }
+
+
+        public void Dispose() {
+            Target.Dispose();
+        }
+
+
+        public async Task WriteAsync(IEnumerable<ILexical> resultSet, string resultSetTitle, DegreeOfOutput degreeOfOutput) {
+            await Target.WriteStringAsync(Serialize(resultSet, resultSetTitle, degreeOfOutput).ToString(SaveOptions.None));
+        }
+    }
+}
