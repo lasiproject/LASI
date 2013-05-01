3fcfaedc1d5a4dcafb22d5ae15d4cac94253b2dc
eroger <eroger@49725315-0998-4498-bb4a-373bc1a52cb2>
2013-05-01 08:09:41 +0000

    Trying to make a "master" collection container for each document so that I can remove the duplicate words, combine them into an "all documents master", and figure out the IDF.  But, no matter which collection type I use, I get an error that I cannot resolve.
    I tried using Collections, Enumerables, Dictionaries, and finally resorted to Lists but can't seem to get any working.
    
    If you could help me out, the error is on line 65 currently.  I need to take a break.. but once this is figured out I should be able to knock it out.  I thought I would have it by now; I just don't understand what the error messages are hinting at.
    
    git-svn-id: http://vps6982.inmotionhosting.com/svn/lasi@640 49725315-0998-4498-bb4a-373bc1a52cb2

--- IndividualProjects/Erik_Experimentation/Program.cs
+++ IndividualProjects/Erik_Experimentation/Program.cs
@@ -21,7 +21,13 @@ using LASI.Algorithm.DocumentConstructs;
 
 namespace Erik_Experimentation
 {
-    class Program
+
+    public class Track
+    {
+        public Word word { get; set; }
+        public int freq { get; set; }
+    }
+    public class Program
     {
 
         static void Main(string[] args) {
@@ -48,15 +54,15 @@ namespace Erik_Experimentation
 
 
 
-            var document1 = TaggerUtil.LoadTextFile(new TextFile(@"C:\Users\Aluan\Desktop\weight2.txt"));
-            var document2 = TaggerUtil.LoadTextFile(new TextFile(@"C:\Users\Aluan\Desktop\weight3.txt"));
+            var document1 = TaggerUtil.LoadTextFile(new TextFile(@"C:\Users\CynosureEPR\Desktop\weight2.txt"));
+            var document2 = TaggerUtil.LoadTextFile(new TextFile(@"C:\Users\CynosureEPR\Desktop\weight3.txt"));
 
             var documents = new Document[] { document1, document2 };
 
-
+            //List<Dictionary<String, int>> master = new List<Dictionary<string, int>>();
+            List<Track> master = new List<Track>();
             foreach (var d in documents) {
-                InverseDocumentFrequency(d);
-
+                master.Add(IndividualDocumentFrequency(d));
             }
 
 
@@ -75,118 +81,71 @@ namespace Erik_Experimentation
 
         }
 
-        private static void InverseDocumentFrequency(Document d) {
+     
+        public static List<Track> IndividualDocumentFrequency(Document d) {
 
+            //Dictionary<String, int> wordsIndex = new Dictionary<String, int>();
+            List<Track> wordsIndex2 = new List<Track>();
+            IEnumerable<int> allWordCountsE = from searchWord in d.Words
+                                             select d.Words.Count(wordToCompare => wordToCompare.Text == searchWord.Text);
 
-            //IEnumerable<int> allWordCounts = from searchWord in d.Words
-            //                                 select d.Words.Count(wordToCompare => wordToCompare.Text == searchWord.Text);
+          
 
-            foreach (Word w in d.Words) {
-                w.FrequencyCurrent = d.Words.Count(wordCompare => wordCompare.Text == w.Text);
+            List<int> allWordCounts = allWordCountsE.ToList();
+            List<Word> allWords = d.Words.ToList();
+            
+            for(int i = 0; i < d.Words.Count(); i++){
 
-                Console.WriteLine("Word : " + w + "Count: " + w.FrequencyCurrent);
+                wordsIndex2.Add(new Track
+                {
+                    word = allWords[i],
+                    freq = allWordCounts[i]
+                });
+
+                //wordsIndex.Add(allWords[i].Text, allWordCounts[i]); DICTIONARY WAS WRONG YOU IDIOT
+
             }
+            
+            //foreach (Word w in d.Words)
+            //{
 
+            //    foreach (var i in allWordCounts)
+            //    {
+            //        wordsIndex.Add(w.Text, i);
+            //    }
+            //}
+
+            
+            //foreach (KeyValuePair<string, int> pair in wordsIndex)
+            //{
+            //    Console.WriteLine("{0}, {1}",
+            //    pair.Key,
+            //    pair.Value);
+            //}
+
+            return wordsIndex2;
+            
 
 
 
         }
+        //static int Count(this IEnumerable<Word> someWords, Func<Word, bool> comparisonFunction) {
+        //    int cnt = 0;
+        //    foreach (var word in someWords) {
+        //        if (comparisonFunction(word)) {
+        //            cnt++;
+        //        }
+        //    }
+        //    return cnt;
+        //}
+
+
+
 
     }
 
 
-    /// <summary>
-    /// Attempts to show how functions which take other functions, such as lambda expressions, as parameters make use of those functions.
-    /// Contains several example functions which take other functions as parameters.
-    /// These types of functions are called higher order functions, because they take other functions, allowing them to abstract over common activities such as counting.
-    /// 
-    /// 
-    /// Count is a kind of pattern which can roughly be described by the following two scenarios:
-    /// 
-    /// Scenario 1.
-    /// 
-    ///         Given some sequence of elements    
-    ///                 ex: think vector, set, sequence, iterable, enumerable
-    /// 
-    ///         Visit each element in said sequence, making a note of its existence in a tally
-    ///                 
-    /// Scenario 2.
-    /// 
-    ///         Given some sequence of element    
-    ///             
-    ///         select one or more criteria which can be relevantly tested for on any element in said sequence
-    ///                 
-    ///         Visit each element in said sequence, only making a note of its existence, if it matches the criteria specified
-    /// 
-    ///         
-    /// </summary>
-    internal static class FunctionsTakingFunctionAsAgrumentsExamples
-    {
-        /// <summary>
-        /// Scenario 1.
-        /// </summary>
-        static int Count(this IEnumerable<Word> someWords) {
-            int cnt = 0;
-            foreach (var word in someWords) {
-                cnt++;
-            }
-            return cnt;
-        }
-        /// <summary>
-        /// Scenario 2.
-        /// </summary>
-        static int Count(this IEnumerable<Word> someWords, Func<Word, bool> comparisonFunction) {
-            int cnt = 0;
-            foreach (var word in someWords) {
-                if (comparisonFunction(word)) {
-                    cnt++;
-                }
-            }
-            return cnt;
-        }
 
-
-        /// <summary>
-        /// Example with data
-        /// </summary>
-        static void DrugsAreInteresting() {
-
-
-            var someDrugs = new[] {
-                new { Name = "LSD", Category = "Hallucinogen" },
-                new { Name = "Aderal", Category = "Amphetamine" },
-                new { Name = "Vyvanse", Category = "Amphetamine" },                    
-                new { Name = "Zoloft", Category = "SSRI" } 
-            };
-
-            var someGuy = new {
-                FirName = "John",
-                LastName = "Smith",
-                DrugPreferences = new[] {
-                    new { Category = "Amphetamine", PreferenceLevel = 1 },
-                    new { Category = "Hallucinogen", PreferenceLevel = 2} 
-                },
-                GetHigh = new Func<dynamic, dynamic>(drug => "Some Guy is now high on " + drug.Name)
-            };
-
-            var defaultStates = new[] { "sober", "moody" };
-
-            var drugsSomeGuyWillLike =
-                someDrugs
-                .Where(drug => someGuy.DrugPreferences
-                    .Any(dr => dr.Category == drug.Category));
-
-            var canGetHighToday = drugsSomeGuyWillLike.Count() > 0;
-
-
-        }
-    }
 }
-
-
-
-
-
-
 
 
