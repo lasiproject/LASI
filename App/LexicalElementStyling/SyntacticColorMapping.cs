e95a6dfea3ef391d858044214dfeab157fd391de
Aluan Haddad <aluanh@gmail.com>
2014-01-15 20:47:12 -0500

    Abstracted the syntactic color mapping pattern. Abstracted the syntactic color mapping pattern by creating a new interface, ISyntacticColorizer<TLexical, TColoration>, defining it in the Interop assembly for easy extensibility. Refactored the Wpf code which defined some prelimenary coloration by syntactic definition from a static method to a class implementing the aforementioned interface.

--- App/LexicalElementStyling/SyntacticColorMapping.cs
+++ App/LexicalElementStyling/SyntacticColorMapping.cs
@@ -1,38 +1,43 @@
 ﻿using LASI.Core;
 using LASI.Core.Patternization;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Media;
 
 namespace LASI.App
 {
-    static class SyntacticStylization
+    class SyntacticColorMap : LASI.Interop.ISyntacticColorizer<ILexical, Brush>
     {
         /// <summary>
         /// Maps a Lexical element to a syntax highlighting color. The returned value is a System.Windows.Media.Brush enumeration member.
         /// </summary>
-        /// <param name="syntactic">The Lexical for which to get a color based on its syntactic role.</param>
+        /// <param name="syntacticElement">The Lexical for which to get a color based on its syntactic role.</param>
         /// <returns>A System.Windows.Media.Brush enumeration value mapped to the syntactic role of the element.</returns>
-        public static Brush GetSyntacticColorization(this ILexical syntactic) {
-            return syntactic.Match().Yield<Brush>().
-                    With<Phrase>(p => p.Match().Yield<Brush>().
-                            With<PronounPhrase>(Brushes.HotPink).
-                            When<NounPhrase>(n => n.Words.OfProperNoun().Any()).
-                            Then(Brushes.DarkBlue).
-                            With<NounPhrase>(Brushes.MediumTurquoise).
-                            With<InfinitivePhrase>(Brushes.Teal).
-                            With<IReferencer>(Brushes.DarkCyan).
-                            With<IEntity>(Brushes.DeepSkyBlue).
-                            With<IVerbal>(Brushes.Green).
-                            With<IPrepositional>(Brushes.DarkOrange).
-                            With<IDescriptor>(Brushes.Indigo).
-                            With<IAdverbial>(Brushes.Orange).
-                        Result(Brushes.Black)).
-                    With<Word>(w => w.Match().Yield<Brush>().
-                            With<Adjective>(Brushes.Indigo).
-                            With<PresentParticipleGerund>(Brushes.DarkGreen).
-                            With<Verb>(Brushes.Green).
-                        Result(Brushes.Black)).
-                    Result();
+        public Brush this[ILexical syntacticElement] {
+            get {
+                return syntacticElement.Match().Yield<Brush>().
+                  With<Phrase>(p => p.Match().Yield<Brush>().
+                          With<PronounPhrase>(Brushes.HotPink).
+                          When<NounPhrase>(n => n.Words.OfProperNoun().Any()).
+                          Then(Brushes.DarkBlue).
+                          With<NounPhrase>(Brushes.MediumTurquoise).
+                          With<InfinitivePhrase>(Brushes.Teal).
+                          With<IReferencer>(Brushes.DarkCyan).
+                          With<IEntity>(Brushes.DeepSkyBlue).
+                          With<IVerbal>(Brushes.Green).
+                          With<IPrepositional>(Brushes.DarkOrange).
+                          With<IDescriptor>(Brushes.Indigo).
+                          With<IAdverbial>(Brushes.Orange).
+                      Result(Brushes.Black)).
+                  With<Word>(w => w.Match().Yield<Brush>().
+                          With<Adjective>(Brushes.Indigo).
+                          With<PresentParticipleGerund>(Brushes.DarkGreen).
+                          With<Verb>(Brushes.Green).
+                      Result(Brushes.Black)).
+                  Result();
+            }
         }
     }
+
+
 }
