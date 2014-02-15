cecd621fe0ad1ed31ac5c6179c9ea0af6151f50f
androiddummy <dustinp@lasi-project.org>
2014-02-15 08:20:05 -0500

    Fixed an error in the Example.cshtml file where an IsUpper function was called with no parameters. Used the "this" parameter to satisfy the goal of the Lambda function. Also added an Index.js file for all javascript content in the Index file.

---
+++ MvcApplication2/Scripts/app/Index/Index.js
@@ -0,0 +1,4 @@
+﻿/*This file will contain all the javascript and jquery functions for Index.cshtml. 
+Keeping the functions will help with organization and will allow us to create classes with collections of javascript functions and that way we can load those classes, 
+which will optimize page load time. 
+*/
