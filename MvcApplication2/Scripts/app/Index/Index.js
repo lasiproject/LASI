6c445dcdc47d1db6994fa4c1ea9e86449e20ec3c
androiddummy <dpatr004@gmail.com>
2014-02-16 20:15:56 -0500

    Fixed bootstrap code on general layout page

--- MvcApplication2/Scripts/app/Index/Index.js
+++ MvcApplication2/Scripts/app/Index/Index.js
@@ -2,3 +2,18 @@
 Keeping the functions will help with organization and will allow us to create classes with collections of javascript functions and that way we can load those classes, 
 which will optimize page load time. 
 */
+
+//This function disables submit button 
+$(document).ready(
+                function () {
+                    $('input:submit').attr('disabled', true);
+                    $('input:file').change(
+                        function () {
+                            if ($(this).val()) {
+                                $('input:submit').removeAttr('disabled');
+                            }
+                            else {
+                                $('input:submit').attr('disabled', true);
+                            }
+                        });
+                });
