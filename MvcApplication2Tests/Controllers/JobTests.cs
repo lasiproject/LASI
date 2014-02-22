e1663a77fc4ec035f4a21851e301b7099e2d41dd
Aluan Haddad <aluanh@gmail.com>
2014-02-22 02:55:46 -0500

    Added a standard message to provide job status replacing an anonymous type for the sake of documentation and testability. Implemented Unit tests for the class, which manages its own serialization to JSON.

---
+++ MvcApplication2Tests/Controllers/JobTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LASI.WebService.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LASI.WebService.Controllers.Tests
+{
+    [TestClass()]
+    public class JobTests
+    {
+        [TestMethod()]
+        public void JobTest() {
+            var job = new Job("parsing", 10);
+            Assert.AreEqual(job.PercentComplete, 10d);
+            Assert.AreEqual(job.CurrentOperation, "parsing");
+        }
+
+        [TestMethod()]
+        public void ToJsonTest() {
+            var job = new Job("parsing", 10.0);
+            Assert.AreEqual(string.Format("{{\"JobId\":\"{0}\",\"CurrentOperation\":\"{1}\",\"PercentComplete\":{2:0.0}}}",
+                job.JobId,
+                job.CurrentOperation,
+                job.PercentComplete),
+            job.ToJson());
+        }
+    }
+}
---
+++ MvcApplication2Tests/Controllers/StatusControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LASI.WebService.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LASI.WebService.Controllers.Tests
+{
+    [TestClass()]
+    public class StatusControllerTests
+    {
+        [TestMethod()]
+        public void GetJobStatusTest() {
+            Assert.Fail();
+        }
+    }
+}
