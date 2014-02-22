09313d56e83d668c6fceb0d00061346424aa3a53
androiddummy <dpatr004@gmail.com>
2014-02-22 15:37:06 -0500

    Implementing a model for account management

---
+++ MvcApplication2/Models/LasiAccountModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace LASI.WebService.Models.Login
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
