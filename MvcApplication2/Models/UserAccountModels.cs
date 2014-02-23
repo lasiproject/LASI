513d869446e613969e4ed2e9c0a26943817fc8fa
androiddummy <dustinp@lasi-project.org>
2014-02-23 08:35:41 -0500

    Created a "Create Account" view with a fillable form. Redesigned URL Slugs to make more sense.

--- MvcApplication2/Models/UserAccountModels.cs
+++ MvcApplication2/Models/UserAccountModels.cs
@@ -6,10 +6,13 @@ using System.Data.Entity;
 using System.Globalization;
 using System.Web.Security;
 
-namespace LASI.WebService.Models.Login
+namespace LASI.WebService.Models.User
 {
-    public class LoginModel
+    public class UserModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Organization { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
