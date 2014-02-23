09de832252486f12c089b8eb4883f93b0de8050a
Aluan Haddad <aluanh@gmail.com>
2014-02-23 03:41:25 -0500

    Fixed a bug in pagenate which caused an infinite series of empty pages to be generated when a single paragraph exceded the specified size options.

--- UnitTests/TestHelpers/CollectionHelpers.cs
+++ UnitTests/TestHelpers/CollectionHelpers.cs
@@ -12,8 +12,17 @@ namespace LASI.UnitTests.TestHelpers
 {
     static class AssertHelper
     {
-        public static void AreSequenceEqual<T>(IEnumerable<T> first, IEnumerable<T> second) {
-            Assert.IsTrue(first.SequenceEqual(second));
+        public static void AreSequenceEqual<T>(IEnumerable<T> expected, IEnumerable<T> actual) {
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+        public static void AreSequenceEqual<T>(IEnumerable<T> expected, IEnumerable<T> actual, IEqualityComparer<T> comparer) {
+            Assert.IsTrue(expected.SequenceEqual(actual, comparer));
+        }
+        public static void AreSetEqual<T>(IEnumerable<T> expected, IEnumerable<T> actual) {
+            Assert.IsTrue(expected.Except(actual).None());
+        }
+        public static void AreSetEqual<T>(IEnumerable<T> expected, IEnumerable<T> actual, IEqualityComparer<T> comparer) {
+            Assert.IsTrue(expected.Except(actual, comparer).None());
         }
         //public static void AreSequenceEqual<T>(IEnumerable<T> first, IEnumerable<T> second, string message) {
         //    try {
