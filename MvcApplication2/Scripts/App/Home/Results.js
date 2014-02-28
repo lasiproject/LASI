44fbec7a9724d85f18a7602ff7154ae5bcde6362
androiddummy <dpatr004@gmail.com>
2014-02-28 06:04:03 -0500

    Added a mock top results graph using the example from Google Charts.

--- MvcApplication2/Scripts/App/Home/Results.js
+++ MvcApplication2/Scripts/App/Home/Results.js
@@ -63,3 +63,38 @@
     //We can work together on friday to fix it if you haven't figured it out by then. 
 
 }());
+
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1.0', { 'packages': ['corechart'] });
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(drawChart);
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Mushrooms', 3],
+      ['Onions', 1],
+      ['Olives', 1],
+      ['Zucchini', 1],
+      ['Pepperoni', 2]
+    ]);
+
+    // Set chart options
+    var options = {
+        'title': 'Top Results',
+        'width': 500,
+        'height': 1500
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.BarChart(document.getElementById("top-results-graph"));
+    chart.draw(data, options);
+}
