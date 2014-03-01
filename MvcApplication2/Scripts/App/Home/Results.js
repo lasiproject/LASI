70744209ad95ed3919f7b3bae55b92ca930fa013
androiddummy <dpatr004@gmail.com>
2014-03-01 17:03:50 -0500

    Fixed the Results chart to make it work with multiple document modals.

--- MvcApplication2/Scripts/App/Home/Results.js
+++ MvcApplication2/Scripts/App/Home/Results.js
@@ -1,4 +1,6 @@
-﻿(function () {
+﻿// Load the Visualization API and the piechart package.
+google.load('visualization', '1.0', { 'packages': ['corechart'] });
+(function () {
     "use strict";
     $('#myTab a').click(function (e) {
         e.preventDefault();
@@ -55,5 +57,46 @@
             contextMenu.hide();
 
         });
+        var drawChart = function (id) {
+            return function () {
+                // Create the data table.
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'Topping');
+                data.addColumn('number', 'Slices');
+                data.addRows([
+                  ['Mushrooms', 3],
+                  ['Onions', 1],
+                  ['Olives', 1],
+                  ['Zucchini', 1],
+                  ['Pepperoni', 2]
+                ]);
+
+                // Set chart options
+                var options = {
+                    'title': 'Top Results',
+                    'width': 500,
+                    'height': 500
+                };
+
+                // Instantiate and draw our chart, passing in some options.
+                var chart = new google.visualization.BarChart(document.getElementById(id));
+                chart.draw(data, options);
+            };
+        };
+        var charts = $(".chart-container");
+        charts.each(function (index, element) {
+            google.setOnLoadCallback(drawChart(element.id))
+        });
     });
+    // Set a callback to run when the Google Visualization API is loaded.
+    //google.setOnLoadCallback(drawChart);
+
+    //on('shown', function () {
+    //    google.maps.event.trigger(map, "resize");
+    //});
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+
 }());
+
