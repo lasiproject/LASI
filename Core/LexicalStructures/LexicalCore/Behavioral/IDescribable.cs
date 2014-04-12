ca280dd77fb00f81e337a925119d7a7920a98fac
Aluan Haddad <aluanh@gmail.com>
2014-04-12 02:34:03 -0400

    Removed the Idesciblble interface, moving its members to IEntity.

--- Core/LexicalStructures/LexicalCore/Behavioral/IDescribable.cs
+++ Core/LexicalStructures/LexicalCore/Behavioral/IDescribable.cs
@@ -1,30 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
 
-namespace LASI.Core
-{
-    /// <summary>
-    /// <para>Defines the role requirements for Describable constructs, often Nouns or NounPhrases, which can be be modified by any number of descriptive constructs.</para>
-    /// <para>Along with the other interfaces in the Syntactic Interfaces Library, the IDescribable interface provides for generalization and abstraction over word and Phrase types.</para>
-    /// </summary>
-    /// <see cref="IDescriptor"/>
-    public interface IDescribable : ILexical
-    {
-        /// <summary>
-        /// Binds an IDescriptor, generally an Adjective or AdjectivePhrase, as a descriptor of the IDescribable.
-        /// </summary>
-        /// <param name="descriptor">The IDescriptor instance which will be added to the Noun's descriptors.</param>
-        void BindDescriptor(IDescriptor descriptor);
-        /// <summary>
-        /// Gets all of the IDescriptor constructs,generally Adjectives or AdjectivePhrases, which describe the IDescibable.
-        /// </summary>
-        IEnumerable<IDescriptor> Descriptors {
-            get;
-        }
-    }
+//namespace LASI.Core
+//{
+//    /// <summary>
+//    /// <para>Defines the role requirements for Describable constructs, often Nouns or NounPhrases, which can be be modified by any number of descriptive constructs.</para>
+//    /// <para>Along with the other interfaces in the Syntactic Interfaces Library, the IDescribable interface provides for generalization and abstraction over word and Phrase types.</para>
+//    /// </summary>
+//    /// <see cref="IDescriptor"/>
+//    public interface IDescribable : ILexical
+//    {
+//        /// <summary>
+//        /// Binds an IDescriptor, generally an Adjective or AdjectivePhrase, as a descriptor of the IDescribable.
+//        /// </summary>
+//        /// <param name="descriptor">The IDescriptor instance which will be added to the Noun's descriptors.</param>
+//        void BindDescriptor(IDescriptor descriptor);
+//        /// <summary>
+//        /// Gets all of the IDescriptor constructs,generally Adjectives or AdjectivePhrases, which describe the IDescibable.
+//        /// </summary>
+//        IEnumerable<IDescriptor> Descriptors {
+//            get;
+//        }
+//    }
 
 
 
-}
+//}
