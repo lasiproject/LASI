c3a297e95669fcc866cf80f9a1677f8d460696da
aluanhaddad <aluanh@gmail.com>
2014-07-14 01:29:38 -0400

    Some improvemnts on the front end. Began refactoring the Results page to make use of partial views for better. Updated typescript definitions for jquery. Cleaned up some documentation.

--- WebApp/StringExtensions.cs
+++ WebApp/StringExtensions.cs
@@ -12,8 +12,12 @@ namespace LASI.WebApp
         /// engine have been replaced with a single distinct character, delimited by an underscore on either side.
         /// </summary>
         /// <param name="value">The string to transform.</param>
-        /// <returns>A new string in which all characters which are known to be problematic for jQuery's Sizzle Selector engine are replaced with underscores.</returns>
-        /// <remarks>The extra underscores which pad the replacement are costly but allow for the name to remain legible. Note that a space is replaced by a single underscore.</remarks>
+        /// <returns>A new string in which all characters which are known to be problematic for jQuery's Sizzle Selector engine are replaced with underscores.
+        /// </returns>
+        /// <remarks>
+        /// The extra underscores which pad the replacement are costly but allow for the name to remain legible.
+        /// Note that a space is replaced by a single underscore.
+        /// </remarks>
         public static string ToSizzleSafeString(this string value) {
             return value.Replace(' ', '_')
                         .Replace("(", "_a_")
