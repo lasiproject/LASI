2381267576dba02eeceef0f3f9642cb00c9c7877
aluanhaddad <aluanh@gmail.com>
2014-07-15 02:58:03 -0400

    Upgraded all projects in the solution to target .NET 4.5.2. Factored WebApp paragraph rendering into a partial view.

--- WebApp/Models/Home/FileUploadModel.cs
+++ WebApp/Models/Home/FileUploadModel.cs
@@ -11,8 +11,12 @@ namespace LASI.WebApp.Models
     {
         public string Message { get; set; }
         public double Percentage { get; set; }
-
         public string FileName { get; set; }
-
+        /// <summary>
+        /// Gets or sets the file extensions which will be allowed by the upload model.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> AcceptedExtensions { get; set; }
+        public string UploadTarget { get; set; }
     }
 }
