68e5f57dce2458b633d92e0e6f7d78967ddb162d
aluanhaddad <aluanh@gmail.com>
2014-10-18 18:55:31 -0400

    Added some unversioned files.

---
+++ ContentSystem/Serialization/Json/SimpleJsonSerializer.cs
@@ -0,0 +1,31 @@
+﻿using LASI;
+using LASI.Utilities;
+using LASI.Core;
+using LASI.Core.PatternMatching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using LASI.ContentSystem.Serialization.Json;
+
+namespace LASI.ContentSystem.Serialization
+{
+    /// <summary>
+    /// Provides basic Xml serialization of for various configuration of ILexical elements.
+    /// </summary>
+    public class SimpleJsonSerializer : ILexicalSerializer<ILexical, JToken>
+    {
+        /// <summary>
+        /// Serializes the provided sequence of ILexical instances into xml elements and returns a single XElement containing them.
+        /// </summary>
+        /// <param name="source">The sequence of ILexical instances to serialize into a single XElement .</param>
+        /// <param name="parentElementTitle">The desired name for the resulting XElement .</param>
+        /// <param name="degreeOfOutput">The DegreeOfOutput value specifying the per element amount of detail the serilization will retain.</param>
+        /// <returns>A single XElement  containing the serialized representation of the given sequence of elements.</returns>
+        public JToken Serialize(IEnumerable<ILexical> source, string parentElementTitle, DegreeOfOutput degreeOfOutput) {
+            return new JArray(source.Select(SerializationExtensions.ToJObject));
+        }
+    }
+}
---
+++ UnitTests/Serialization/Json/SerializationExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LASI.ContentSystem.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LASI.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LASI.ContentSystem.Serialization.Json.Tests
+{
+    [TestClass]
+    public class SerializationExtensionsTests
+    {
+        [TestMethod]
+        public void ToJArrayTest() {
+            var target = InternalHelpers.GetLexicalSequence();
+            var result = target.ToJArray();
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ToJObjectTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ToJObjectTest1() {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ToJObjectTest2() {
+            Assert.Fail();
+        }
+        private static class InternalHelpers
+        {
+            public static JsonSerializerSettings SerializerSettings {
+                get {
+                    return new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+                }
+            }
+            public static IEnumerable<ILexical> GetLexicalSequence() {
+                foreach (var lexical in new[] { NounPhrase target = new NounPhrase(new ProperPluralNoun("Americans"), new Conjunction("and"), new ProperPluralNoun("Canadians")) }) { yield return lexical; }
+            }
+
+        }
+    }
+}
