fd7ba90791e7e3cfae7611d775387520c901366e
aluanhaddad <aluanh@gmail.com>
2014-12-07 16:15:59 -0500

    Added guards

--- Core/Analysis/PatternMatching/LexicalSpecific/Experimental/TokenBased.cs
+++ Core/Analysis/PatternMatching/LexicalSpecific/Experimental/TokenBased.cs
@@ -10,9 +10,16 @@ namespace LASI.Core.Analysis.PatternMatching.LexicalSpecific.Experimental.TermBa
     {
         public abstract bool AppliesTo<TCase>(TCase value);
     }
-    abstract class LexicalToken<TLexical> : Token where TLexical : class, ILexical
+    abstract class LexicalToken : Token
     {
-        public override bool AppliesTo<TCase>(TCase value) => value is TLexical;
+    }
+    abstract class LexicalToken<TLexical> : LexicalToken where TLexical : class, ILexical
+    {
+        private bool guardFailed;
+
+        public override bool AppliesTo<TCase>(TCase value) => !guardFailed && value is TLexical;
+        public static LexicalToken<TLexical> operator &(LexicalToken<TLexical> token, bool guard) { token.guardFailed = !guard; return token; }
+        public static LexicalToken<TLexical> operator &(LexicalToken<TLexical> token, Func<bool> guard) { token.guardFailed = !guard(); return token; }
     }
     abstract class ValueToken<TValue> : Token
     {
@@ -22,5 +29,92 @@ namespace LASI.Core.Analysis.PatternMatching.LexicalSpecific.Experimental.TermBa
             this.value = value;
         }
         public override bool AppliesTo<TCase>(TCase value) => this.value.Equals(value);
+    }
+
+    class TextToken : ValueToken<string>
+    {
+        public TextToken(string value) : base(value) {
+        }
+        public static implicit operator TextToken(string text) => new TextToken(text);
+    }
+    class PhraseToken : LexicalToken<Phrase> { }
+    class WordToken : LexicalToken<Word> { }
+    class VerbalToken : LexicalToken<IVerbal> { }
+    class EntityToken : LexicalToken<IEntity> { }
+    class ReferencerToken : LexicalToken<IReferencer>
+    {
+
+    }
+
+    class Pattern<TResult>
+    {
+        private TResult result = default(TResult);
+        private readonly ILexical value;
+
+        public Pattern(ILexical value) {
+            this.value = value;
+        }
+
+        public static TokenTestResult operator |(Pattern<TResult> pattern, TextToken token) {
+            return new TokenTestResult(matched: token.AppliesTo(pattern.value.Text), pattern: pattern);
+        }
+        public static TokenTestResult operator |(Pattern<TResult> pattern, LexicalToken token) {
+            return new TokenTestResult(matched: token.AppliesTo(pattern.value.Text), pattern: pattern);
+        }
+        public static TResult operator |(Pattern<TResult> pattern, Func<TResult> f) {
+            return f();
+        }
+
+        public class TokenTestResult
+        {
+            private bool matched;
+            private Pattern<TResult> pattern;
+            private Func<TResult> deferred;
+
+            public TokenTestResult(bool matched, Pattern<TResult> pattern) {
+                this.matched = matched;
+                this.pattern = pattern;
+            }
+
+            public static Pattern<TResult> operator |(TokenTestResult tokenTestResult, Func<TResult> f) {
+                if (tokenTestResult.matched) {
+                    tokenTestResult.matched = true;
+                    tokenTestResult.pattern.result = f();
+                }
+                return tokenTestResult.pattern;
+            }
+            public static Pattern<TResult> operator |(TokenTestResult tokenTestResult, Func<ILexical, TResult> f) {
+                if (tokenTestResult.matched) {
+                    tokenTestResult.matched = true;
+                    tokenTestResult.deferred = () => tokenTestResult.pattern.result = f(tokenTestResult.pattern.value);
+                }
+                return tokenTestResult.pattern;
+            }
+            public static Pattern<TResult> operator &(TokenTestResult tokenTestResult, Func<ILexical, bool> f) {
+                if (tokenTestResult.matched) {
+                    tokenTestResult.matched = f(tokenTestResult.pattern.value);
+                }
+                if (tokenTestResult.matched) { tokenTestResult.pattern.result = tokenTestResult.deferred(); }
+                return tokenTestResult.pattern;
+            }
+        }
+    }
+    static class PatternTestingTokenTestingTest
+    {
+        public static readonly VerbalToken Verbal = new VerbalToken();
+        public static readonly EntityToken Entity = new EntityToken();
+        public static readonly ReferencerToken Referencer = new ReferencerToken();
+        static void Test1(IVerbal verbal) {
+            var textToken = new TextToken("test");
+            var f = new Func<bool>(() => false);
+            var pattern = new Pattern<bool>(verbal);
+
+            var x = pattern
+                | Verbal & verbal.Text == "abc" & (() => 1 == 1) | (() => false)
+                | textToken | (() => true)
+                | Entity | (() => 1 == 4)
+                | Verbal | (v => false)
+                | (() => true);
+        }
     }
 }
---
+++ Utilities/CharacterExtensions.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LASI
+{
+    /// <summary>
+    /// Defines various useful methods for working with characters.
+    /// </summary>
+    public static class CharExtensions
+    {
+        /// <summary>
+        /// Returns a value indicating whether the character is a valid letter.
+        /// </summary>
+        /// <param name="c">
+        /// The character to test.
+        /// </param>
+        /// <returns>
+        /// True if the character is a letter; otherwise, false.
+        /// </returns>
+        public static bool IsAlphabetic(this char c) => c > 96 && c < 123 || c > 64 && c < 91;
+
+        /// <summary>
+        /// Returns a value indicating whether the character is a consonant.
+        /// </summary>
+        /// <param name="c">
+        /// The character to test.
+        /// </param>
+        /// <returns>
+        /// True if the character is a consonant; otherwise, false.
+        /// </returns>
+        public static bool IsConsonant(this char c) => c == 'y' || c == 'Y' || c.IsAlphabetic() && !c.IsVowel();
+
+        /// <summary>
+        /// Returns a value indicating whether the character is a lowercase letter.
+        /// </summary>
+        /// <param name="c">
+        /// The character to test.
+        /// </param>
+        /// <returns>
+        /// True if the character is a lowercase letter; otherwise, false.
+        /// </returns>
+        public static bool IsLower(this char c) => char.IsLower(c);
+
+        /// <summary>
+        /// Returns a value indicating whether the character is an uppercase letter.
+        /// </summary>
+        /// <param name="c">
+        /// The character to test.
+        /// </param>
+        /// <returns>
+        /// True if the character is an uppercase letter; otherwise, false.
+        /// </returns>
+        public static bool IsUpper(this char c) => char.IsUpper(c);
+
+        /// <summary>
+        /// Returns a value indicating whether the character is a vowel.
+        /// </summary>
+        /// <param name="c">
+        /// The character to test.
+        /// </param>
+        /// <returns>
+        /// True if the character is a vowel; otherwise, false.
+        /// </returns>
+        public static bool IsVowel(this char c) => VOWELS.IndexOf(c) >= 0;
+
+        /// <summary>
+        /// Converts the value of the Unicode character to its lowercase equivalent.
+        /// </summary>
+        /// <param name="c">
+        /// The Unicode character to convert.
+        /// </param>
+        /// <returns>
+        /// The lowercase equivalent of c, or the unchanged value of c, if c is already lowercase or
+        /// not alphabetic.
+        /// </returns>
+        public static char ToLower(this char c) => char.ToLower(c);
+
+        /// <summary>
+        /// Converts the value of the Unicode character to its uppercase equivalent.
+        /// </summary>
+        /// <param name="c">
+        /// The Unicode character to convert.
+        /// </param>
+        /// <returns>
+        /// The uppercase equivalent of c, or the unchanged value of c, if c is already lowercase or
+        /// not alphabetic.
+        /// </returns>
+        public static char ToUpper(this char c) => char.ToUpper(c);
+
+        /// <summary>
+        /// Determines whether two characters are considered equal when viewed in a case Insensitive manner.
+        /// </summary>
+        /// <param name="value">The first character to compare.</param>
+        /// <param name="other">The second character to compare</param>
+        /// <returns>True if the given characters considered are equal; otherwise, false.</returns>
+        /// <remarks>This comparison uses the default culture.</remarks>
+        public static bool EqualsIgnoreCase(this char a, char b) => a.ToUpper() == b.ToUpper();
+
+        private const string VOWELS = "aeiouyAEIOUY";
+    }
+}
