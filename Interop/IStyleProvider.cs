fac8100cc4603afd6b92e40b95329684a64c039b
aluanhaddad <aluanh@gmail.com>
2015-01-20 06:09:20 -0500

    Improved documentation for the IStyleProvider interface and its implementations. Provided rational for the usage of the interface along with guidelines.

--- Interop/IStyleProvider.cs
+++ Interop/IStyleProvider.cs
@@ -12,16 +12,24 @@ namespace LASI.Interop
     /// </summary>
     /// <typeparam name="TLexical">The type of Lexical elements to map from. This type parameter is contravariant. That is,
     /// you can use either the type you specified or any type that is less derived.</typeparam>
-    /// <typeparam name="TStylingArtifact">The type of styling artifacts that are associated to mapped elements. This type parameter is covariant. That is,
-    /// you can use either the type you specified or any type that is more derived.</typeparam>
+    /// <typeparam name="TStylingArtifact">The type of styling artifacts that are associated to mapped elements. 
+    /// This type parameter is covariant. That is, you can use either the type you specified or any type that is more derived.
+    /// </typeparam>
+    /// <remarks>
+    /// Motivation:
+    /// This interface was defined to encourage a common patern in which lexical elements would be styled for display by indexing into the map. 
+    /// For example in the LASI WebApp an implementation of this interface associates CSS classes with syntactic elements.
+    /// See <see cref="LASI.WebApp.SyntacticStyleMap"/> and <see cref="LASI.App.SyntacticColorMap"/> for a concrete examples.
+    /// These implementations use the pattern matching facilities provided by the LASI infrastructure to provide a clean readable style mappings to various types.
+    /// </remarks>
     public interface IStyleProvider<in TLexical, out TStylingArtifact> where TLexical : ILexical
     {
         /// <summary>
         /// Gets a stylization value corresponding to the syntactic nature of the indexing lexical element. 
         /// Implementors should provide a default value as a fall back.
         /// </summary>
-        /// <param name="syntacticElement">The lexical element to map to a stylization value.</param>
+        /// <param name="element">The lexical element to map to a stylization value.</param>
         /// <returns>A stylization value corresponding to the syntactic nature of the indexing lexical element.</returns>
-        TStylingArtifact this[TLexical syntacticElement] { get; }
+        TStylingArtifact this[TLexical element] { get; }
     }
 }
--- WebApp/LexicalElementStyling/SyntacticStyleMap.cs
+++ WebApp/LexicalElementStyling/SyntacticStyleMap.cs
@@ -4,27 +4,32 @@ using System.Web.UI.WebControls;
 
 namespace LASI.WebApp
 {
+    /// <summary>
+    /// Associates synactic elements with CSS styles.
+    /// </summary>
+    /// <see cref="Interop.IStyleProvider{TLexical, TStylingArtifact}"/>
     public class SyntacticStyleMap : Interop.IStyleProvider<ILexical, Style>
     {
         /// <summary>
-        /// Maps a Lexical element to a CSS Style specification based on its syntactic nature. The returned value is a System.Windows.Media.Brush enumeration member.
+        /// Maps a Lexical element to a CSS Style based on its syntactic nature. 
         /// </summary>
-        /// <param name="element">The Lexical for which to get a color based on its syntactic role.</param>
-        /// <returns>A System.Windows.Media.Brush enumeration value mapped to the syntactic role of the element.</returns>
-        public Style this[ILexical element] => new Style
-        {
-            CssClass = element.Match()
-                        .Case((IReferencer r) => "referencer")
-                        .Case((NounPhrase n) => n.Words.OfProperNoun().Any() ? "entity proper" : "entity")
-                        .Case((InfinitivePhrase i) => "infinitive")
-                        .Case((PresentParticiple w) => "present-participle-gerund")
-                        .Case((IEntity e) => "entity")
-                        .Case((IVerbal v) => "verbal")
-                        .Case((IPrepositional p) => "prepositional")
-                        .Case((IDescriptor p) => "descriptor")
-                        .Case((IAdverbial a) => "adverbial")
-                        .Case((IConjunctive c) => "conjunctive")
+        /// <param name="element">The Lexical for which to map to a CSS style ebased on its syntactic role.</param>
+        /// <returns>A <see cref="System.Web.UI.WebControls.Style"/> based on the syntactic role of the element.</returns>
+        public Style this[ILexical element] =>
+            new Style
+            {
+                CssClass = element.Match()
+                    .Case((IReferencer r) => "referencer")
+                    .Case((NounPhrase n) => n.Words.OfProperNoun().Any() ? "entity proper" : "entity")
+                    .Case((InfinitivePhrase i) => "infinitive")
+                    .Case((PresentParticiple w) => "present-participle-gerund")
+                    .Case((IEntity e) => "entity")
+                    .Case((IVerbal v) => "verbal")
+                    .Case((IPrepositional p) => "prepositional")
+                    .Case((IDescriptor p) => "descriptor")
+                    .Case((IAdverbial a) => "adverbial")
+                    .Case((IConjunctive c) => "conjunctive")
                     .Result(() => "lexical-default-style")
-        };
+            };
     }
 }
