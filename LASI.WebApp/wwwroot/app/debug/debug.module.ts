23d0ce7527b480674b7fded4ead4bf63310b788a
Aluan Haddad <aluanh@gmail.com>
2015-10-12 22:41:40 -0400

    Added missing file

---
+++ LASI.WebApp/wwwroot/app/debug/debug.module.ts
@@ -0,0 +1,8 @@
+import { debugPanel } from './debug-panel.directive';
+
+var m: AngularModuleOptions = {
+    name: 'debug',
+    requires: [],
+    directives: { debugPanel }
+};
+export default m;   
