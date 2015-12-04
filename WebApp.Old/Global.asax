69208c7b97f4bd8fb44e82310173c22d606fe474
Aluan Haddad <aluanh@gmail.com>
2015-12-03 23:38:11 -0500

    Removed WebApp.Old directory. Made resource directory path in LASI.WebApp\appsettings.json portable.

--- WebApp.Old/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LASI.WebApp.Old.MvcApplication" Language="C#" %>
--- WebApp.Old/Scripts/app/Home/ContextMenuProvider.d.ts
+++
@@ -1,6 +0,0 @@
-declare module LASI.Results.ContextMenuProvider {
-    function applyMenu(element: JQuery, menuItems: {
-        text: string;
-        action: () => void;
-    }[]): void;
-}
