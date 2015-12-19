bc8fb53050927d5718774f1d95d886a34775804f
Aluan Haddad <aluanh@gmail.com>
2015-12-18 21:12:26 -0500

    Added missing files.

---
+++ LASI.WebApp/wwwroot/app/debug/debug-panel.ts
@@ -0,0 +1,26 @@
+﻿'use strict';
+import template from 'app/debug/debug-panel.html';
+
+class DebugPanelController {
+    static $inject = ['tasksListService'];
+    constructor(tasksListService: TasksListService) {
+
+        tasksListService.getActiveTasks()
+            .then(tasks => this.tasks = tasks.sort((x, y) => x.id.localeCompare(y.id)), console.error.bind(console));
+    }
+
+    get buttonText() { return this.infoVisible ? 'Hide' : 'Show'; }
+
+    toggle() { this.infoVisible = !this.infoVisible; }
+
+    tasks: Task[] = [];
+    infoVisible = false;
+}
+export function debugPanel(): ng.IDirective {
+    return {
+        template,
+        controller: DebugPanelController,
+        bindToController: {},
+        controllerAs: 'debug'
+    };
+}
