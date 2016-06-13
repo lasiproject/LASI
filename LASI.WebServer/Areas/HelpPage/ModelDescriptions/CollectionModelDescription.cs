8ddd74e25f8c828c05dc696dd6a2b24de06eb602
Aluan Haddad <aluanh@gmail.com>
2016-06-12 23:48:47 -0400

    Initial commit

---
+++ LASI.WebServer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace LASI.WebServer.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ LASI.WebServer/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LASI.WebServer.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
