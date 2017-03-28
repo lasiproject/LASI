4fd6eb0e0e896800649dafee7ccda2ef6aa2d1e6
Aluan Haddad <aluanh@gmail.com>
2017-03-28 02:36:55 -0400

    remove old angular 2 files from branch

--- LASI.Ng2App/wwwroot/app/document-viewer/components.ts
+++
@@ -1,5 +0,0 @@
-export * from './phrase/phrase';
-export * from './sentence/sentence';
-export * from './paragraph/paragraph';
-export * from './page/page';
-export * from './document-viewer';
--- LASI.Ng2App/wwwroot/app/templates.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'app/app.html' { export default ''; }
-declare module 'app/login.html' { export default ''; }
-declare module 'app/templates/error-modal.html' { export default ''; }
