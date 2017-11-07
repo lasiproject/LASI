d93a7bca42d58c47569e0f6ceaf821b8cc7be776
Aluan Haddad <aluanh@gmail.com>
2017-11-07 08:42:57 -0500

    Add LASI.WebService (WebApp template with store in app auth used) TODO: strip

---
+++ LASI.WebService/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LASI.WebService.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
